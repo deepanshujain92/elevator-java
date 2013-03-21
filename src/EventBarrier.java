1f664124d892d2f2a9f088cc8fb0553b59f09e50
Jenni Mercado <jdm45@duke.edu>
2013-03-20 23:15:05 -0400

    workin on that eventbarrier

--- src/EventBarrier.java
+++ src/EventBarrier.java
@@ -3,11 +3,13 @@ public class EventBarrier extends AbstractEventBarrier {
 
 	private volatile boolean isSignaled;
 	private int _numWorkers;
+	private int numWaiters;
 	
 	public EventBarrier(int numWorkers) {
 		super(numWorkers);
 		isSignaled = false;
 		_numWorkers = numWorkers;
+		numWaiters = 0;
 		
 	}
 
@@ -16,10 +18,10 @@ public class EventBarrier extends AbstractEventBarrier {
 		// TODO Auto-generated method stub
 		if (isSignaled) {
 			// no need to wait, just continue
-			// numWaiters++ or numWaiters--
+			// numWaiters++;
 		}
 		else {
-			//numWaiters++ or numWaiters--
+			numWaiters++;
 			//wait
 			while (!isSignaled) {
 				this.wait();
@@ -30,22 +32,33 @@ public class EventBarrier extends AbstractEventBarrier {
 	@Override
 	public synchronized void raise() throws InterruptedException {
 		// TODO Auto-generated method stub
-		// notifyAll();
+		isSignaled = true;
+		notifyAll();
 		// wait for all workers to complete?
-
+		while (waiters() > 0) {
+			this.wait(); // wakes up when workers have completed 
+		}
+		isSignaled = false;
+		notifyAll(); // lets completed threads know to continue
 	}
 
 	@Override
 	public synchronized void complete() throws InterruptedException {
 		// TODO Auto-generated method stub
 		//notify the producer that you have ... the bridge
+		numWaiters--;
+		notifyAll();
 		//wait for all workers to complete
+		while (!isSignaled) {
+			this.wait();
+		}
+		
 		
 	}
 
 	@Override
 	public int waiters() {
 		// TODO Auto-generated method stub
-		return 0;
+		return numWaiters;
 	}
 }
