4434343c6a4919b9ad53098a3a34bf2cbd39c387
Jenni Mercado <jdm45@duke.edu>
2013-03-21 23:30:49 -0400

    just a couple skeleton classes

---
+++ src/Building.java
@@ -0,0 +1,21 @@
+
+public class Building extends AbstractBuilding {
+
+	public Building(int numFloors, int numElevators) {
+		super(numFloors, numElevators);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void CallUp(int fromFloor) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void CallDown(int fromFloor) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
