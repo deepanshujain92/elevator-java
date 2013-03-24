957a0ca32dbe73576f7816daae3aebff2704892e
mje15 <trinity.test612@gmail.com>
2013-03-24 01:39:21 -0400

    trying to start Building.java, going poorly

--- src/Building.java
+++ src/Building.java
@@ -1,9 +1,21 @@
 
 public class Building extends AbstractBuilding {
 
+	private EventBarrier[] UpCalls;
+	private EventBarrier[] DownCalls;
+	private EventBarrier[] ExitBarriers;
+	
 	public Building(int numFloors, int numElevators) {
 		super(numFloors, numElevators);
 		// TODO Auto-generated constructor stub
+		UpCalls = new EventBarrier[numFloors];
+		DownCalls = new EventBarrier[numFloors];
+		ExitBarriers = new EventBarrier[numFloors];
+		
+		for (int i = 0; i<numFloors; i++){
+			
+		}
+		
 	}
 
 	@Override
