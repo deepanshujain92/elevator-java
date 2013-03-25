8c92ffa9e4dda4b8e8ecafbba935475ac854f159
Jenni Mercado <jdm45@duke.edu>
2013-03-24 23:45:10 -0400

    look it reads input files now! and can log

--- src/ElevatorTester.java
+++ src/ElevatorTester.java
@@ -1,4 +1,41 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.logging.*;
 
 public class ElevatorTester {
+
+	private static String inputFileName;
+	// use for logging errors, maybe when elevators leave floors and with how
+	// many riders?
+	private static Logger logger = Logger.getLogger("Elevator");
+
+	public ElevatorTester(String filename) {
+		inputFileName = filename;
+	}
+
+	public static void main(String args[]) {
+
+		// take input file and create elevator/rider threads out of it
+		logger.info("Writing to a log file, nbd");
+
+		Path path = Paths.get(inputFileName);
+		try (Scanner scanner = new Scanner(path)) {
+			while (scanner.hasNextLine()) {
+				String info = scanner.nextLine();
+				String[] elevatorParams = info.split(" ");
+				Elevator elevator = new Elevator(
+						Integer.parseInt(elevatorParams[1]),
+						Integer.parseInt(elevatorParams[2]),
+						Integer.parseInt(elevatorParams[3]));
+				new Thread(elevator).start();
+			}
+		} catch (IOException e) {
+			logger.info("File not found");
+			e.printStackTrace();
+		}
+
+	}
 
 }
