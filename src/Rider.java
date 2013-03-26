d96f2cf7429b75a5d33f2eabae4752e64262af73
mje15 <trinity.test612@gmail.com>
2013-03-25 22:39:05 -0400

    Working on Rider

--- src/Rider.java
+++ src/Rider.java
@@ -4,17 +4,28 @@ public class Rider implements Runnable {
 	private int riderID;
 	private int startFloor;
 	private int endFloor;
+	private Building building;
 	
-	public Rider(int ID, int start, int end){
+	public Rider(Building building, int ID, int start, int end){
 		riderID = ID;
 		startFloor = start;
 		endFloor = end;
+		building = building;
 	}
 	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
+		if (startFloor > endFloor){
+			building.CallDown(startFloor);
+		}
+		else{
+			building.CallUp(startFloor);
+		}
 		
+		//call enter() on elevator
+		//call requestFloor(endFloor)
+		//call 
 
 	}
 
