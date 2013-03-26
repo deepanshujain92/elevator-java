35d82d4636d8377f8dec879c877c1d8ba52dabba
mje15 <trinity.test612@gmail.com>
2013-03-25 20:44:40 -0400

    Added Rider class

---
+++ src/Rider.java
@@ -0,0 +1,21 @@
+
+public class Rider implements Runnable {
+
+	private int riderID;
+	private int startFloor;
+	private int endFloor;
+	
+	public Rider(int ID, int start, int end){
+		riderID = ID;
+		startFloor = start;
+		endFloor = end;
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
